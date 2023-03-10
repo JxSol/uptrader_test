36f3dd80a2482f9383e20ad169f0d0c8ab9fb084
JxSol <jxsol.js@gmail.com>
2023-03-10 11:25:32 +0600

    fix admin:tree_menu_menu_add save button

--- tree_menu/admin.py
+++ tree_menu/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path
+from django.shortcuts import redirect
+from django.urls import path, reverse
 
 from .forms import NodeForm, MenuForm
 from .models import Menu
@@ -9,6 +10,11 @@ from .views import clean_node
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
     change_form_template = 'admin/tree_menu/menu_change.html'
+
+    def response_add(self, request, obj, post_url_continue=None):
+        super().response_add(request, obj, post_url_continue=post_url_continue)
+        edit_url = reverse('admin:tree_menu_menu_change', args=[obj.pk])
+        return redirect(edit_url)
 
     def change_view(self, request, object_id, form_url="", extra_context=None):
         extra_context = extra_context or {}
