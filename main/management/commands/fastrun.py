09cd785a7f0ecb2aea91049a6ac54bfa851b3fd2
JxSol <jxsol.js@gmail.com>
2023-03-10 12:03:04 +0600

    add command fastrun

---
+++ main/management/commands/fastrun.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+class Command(BaseCommand):
+    help = 'Fast run project'
+
+    def handle(self, *args, **options):
+        call_command('makemigrations')
+        call_command('migrate')
+        if not User.objects.filter(username='admin').exists():
+            superuser = User.objects.create_superuser('admin', password='admin')
+            print('Суперпользователь "admin" успешно создан')
+        call_command('runserver')
